75c3e4819aa4727c85ea45038edaf05cb5b57210
Connor Stone <connor.stone@gmail.com>
2024-01-09 15:30:36 -0700

    Updated fog logic and added basic traffic logic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,40 +1,76 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public Color clearFog,
+    [SerializeField]
+    Color clearFog,
         hazyFog;
-    public float minHaze,
+
+    [SerializeField]
+    float minHaze,
         maxHaze;
 
+    [Range(0f, 100f)]
+    [SerializeField]
+    float hazeLevel;
+
     float targetHaze;
+
+    [SerializeField]
+    Transform carContainer;
+
+    [SerializeField]
+    [Range(0f, 100f)]
+    float trafficLevel;
+
+    int totalNumCars,
+        currentNumCars;
 
     void Start()
     {
         targetHaze = maxHaze;
+        foreach (Transform carLine in carContainer)
+        {
+            totalNumCars += carLine.childCount;
+        }
+        currentNumCars = totalNumCars;
     }
 
     void Update()
     {
-        var dt = Time.deltaTime;
-        UpdateFog(dt);
+        UpdateFog();
+        UpdateTraffic();
     }
 
-    void UpdateFog(float deltaTime)
+    void UpdateFog()
     {
-        var currentHaze = RenderSettings.fogDensity;
-        if (targetHaze == maxHaze)
-        {
-            RenderSettings.fogDensity =
-                currentHaze < maxHaze ? currentHaze + (0.001f * deltaTime) : currentHaze;
-            targetHaze = currentHaze < maxHaze ? maxHaze : minHaze;
-            return;
-        }
+        RenderSettings.fogDensity = minHaze + (maxHaze * (hazeLevel / 100f));
+        RenderSettings.fogColor = Color.Lerp(clearFog, hazyFog, Math.Clamp(hazeLevel / 50f, 0, 1));
+    }
 
-        RenderSettings.fogDensity =
-            currentHaze > minHaze ? currentHaze - (0.001f * deltaTime) : currentHaze;
-        targetHaze = currentHaze > minHaze ? minHaze : maxHaze;
+    void UpdateTraffic()
+    {
+        var targetNumCars = Math.Clamp(
+            Math.Ceiling(totalNumCars * (trafficLevel / 100f)),
+            10,
+            totalNumCars + 1
+        );
+        while (currentNumCars != targetNumCars)
+        {
+            System.Random rng = new();
+            var carDelta = Math.Sign(targetNumCars - currentNumCars); // returns -1 or 1 to show we are removing or adding cars
+            var enableCars = carDelta > 0; // gets whether we want to enable or disable cars
+            var currentCarLine = carContainer.GetChild(rng.Next(carContainer.childCount)); // gets a random carLine transform
+            var i = enableCars ? currentCarLine.childCount : 1; // determines the direction we enable / disbale cars
+            var currentCar = currentCarLine.GetChild(currentCarLine.childCount - i).gameObject; // gets the current car based on above criteria, first if enabling and last if disabling
+            while (currentCar.activeSelf == enableCars) // find a car that is not in the desired state
+            {
+                i += -carDelta;
+                currentCar = currentCarLine.GetChild(currentCarLine.childCount - i).gameObject;
+            }
+            currentCar.SetActive(enableCars); // enable / disable the found car
+            currentNumCars += carDelta; // increment the number of cars visible
+        }
     }
 }
--- Assets/Scripts/UI/DebugUI.cs
+++ Assets/Scripts/UI/DebugUI.cs
@@ -14,6 +14,9 @@ public class DebugUI : MonoBehaviour
     TMP_Text fpsCounter,
         frameTimeCounter;
 
+    [SerializeField]
+    bool startVisible;
+
     Controls controls;
     InputAction debugShowHideKey,
         debug2DKey,
@@ -22,6 +25,8 @@ public class DebugUI : MonoBehaviour
     void Awake()
     {
         controls = new Controls();
+        hidden = startVisible;
+        debugUIContainer.SetActive(hidden);
     }
 
     // setup event handlers
