748c5aa5cafdd5255b61047aad3fc09c3a179c2c
Connor Stone <connor.stone@gmail.com>
2024-01-12 07:50:19 -0700

    Added basic profit / cost functionality

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,13 @@
 using System;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Pollution Settings")]
+    [SerializeField]
+    TMP_Text Pollution;
+
     [SerializeField]
     Color clearFog,
         hazyFog;
@@ -15,6 +20,10 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     float hazeLevel;
 
+    [Header("Traffic Settings")]
+    [SerializeField]
+    TMP_Text Traffic;
+
     [SerializeField]
     Transform carContainer;
 
@@ -24,6 +33,16 @@ public class GameManager : MonoBehaviour
 
     int totalNumCars,
         currentNumCars;
+
+    [Header("Cost / Profit Settings")]
+    [SerializeField]
+    TMP_Text Cost,
+        Profit;
+
+    [Range(0.00f, 10000000.00f)]
+    [SerializeField]
+    float costLevel,
+        profitLevel;
 
     void Start()
     {
@@ -38,16 +57,20 @@ public class GameManager : MonoBehaviour
     {
         UpdateFog();
         UpdateTraffic();
+        UpdateCost();
+        UpdateProfit();
     }
 
     void UpdateFog()
     {
+        Pollution.text = "Pollution Level: " + hazeLevel;
         RenderSettings.fogDensity = minHaze + (maxHaze * (hazeLevel / 100f));
         RenderSettings.fogColor = Color.Lerp(clearFog, hazyFog, Math.Clamp(hazeLevel / 50f, 0, 1));
     }
 
     void UpdateTraffic()
     {
+        Traffic.text = "Traffic Level: " + trafficLevel;
         var targetNumCars = Math.Clamp(
             Math.Ceiling(totalNumCars * (trafficLevel / 100f)),
             30,
@@ -58,32 +81,41 @@ public class GameManager : MonoBehaviour
             System.Random rng = new();
             var carDelta = Math.Sign(targetNumCars - currentNumCars); // returns -1 or 1 to show we are removing or adding cars
             var enableCars = carDelta > 0; // gets whether we want to enable or disable cars
-            var currentCarLine = carContainer.GetChild(rng.Next(carContainer.childCount)); // gets a random carLine transform
-            var firstCarActive = currentCarLine.GetChild(0).gameObject.activeSelf;
-            var lastCarActive = currentCarLine
-                .GetChild(currentCarLine.childCount - 1)
-                .gameObject.activeSelf;
-            while (
-                (firstCarActive == enableCars && enableCars == false)
-                || (lastCarActive == enableCars && enableCars == true)
-            )
+
+            Transform currentCarLine;
+            bool firstCarActive,
+                lastCarActive;
+            do
             {
                 currentCarLine = carContainer.GetChild(rng.Next(carContainer.childCount)); // gets a random carLine transform
                 firstCarActive = currentCarLine.GetChild(0).gameObject.activeSelf;
                 lastCarActive = currentCarLine
                     .GetChild(currentCarLine.childCount - 1)
                     .gameObject.activeSelf;
-            }
+            } while (
+                (firstCarActive == enableCars && enableCars == false)
+                || (lastCarActive == enableCars && enableCars == true)
+            );
 
             var i = enableCars ? currentCarLine.childCount : 1; // determines the direction we enable / disbale cars
             var currentCar = currentCarLine.GetChild(currentCarLine.childCount - i).gameObject; // gets the current car based on above criteria, first if enabling and last if disabling
             while (currentCar.activeSelf == enableCars) // find a car that is not in the desired state
             {
-                i += -carDelta;
+                i -= carDelta;
                 currentCar = currentCarLine.GetChild(currentCarLine.childCount - i).gameObject;
             }
             currentCar.SetActive(enableCars); // enable / disable the found car
             currentNumCars += carDelta; // increment the number of cars visible
         }
+    }
+
+    void UpdateCost()
+    {
+        Cost.text = "Costs: $" + costLevel;
+    }
+
+    void UpdateProfit()
+    {
+        Profit.text = "Profit: $" + profitLevel;
     }
 }
